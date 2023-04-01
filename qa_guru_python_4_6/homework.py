130ef663153c8501eaeba5f6fca9926b3981240c
DronovNA <nikita.dronov.a@gmail.com>
2023-04-01 18:04:10 +0300

    End homework

--- qa_guru_python_4_6/homework.py
+++ qa_guru_python_4_6/homework.py
@@ -24,8 +24,15 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
-
     is_dark_theme = None
+    if (
+        time(22) <= current_time
+        or current_time <= time(6)
+        or dark_theme_enabled_by_user is True
+    ):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
     assert is_dark_theme is True
 
 
@@ -43,10 +50,13 @@ def test_find_suitable_user():
 
     # TODO найдите пользователя с именем "Olga"
     suiable_user = None
+    for name in users:
+        if (name["name"]) == "Olga":
+            suiable_user = name
     assert suiable_user == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suiable_users = None
+    suiable_users = [age for age in users if age["age"] < 20]
     assert suiable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -64,6 +74,10 @@ def test_find_suitable_user():
 # "Open Browser [Chrome]"
 
 
+def rename_function(func_name, *args):
+    return f'{func_name.__name__.replace("_", " ").title()} [{", ".join(args)}]'
+
+
 def test_readable_function():
     open_browser(browser_name="Chrome")
     go_to_companyname_homepage(page_url="https://companyname.com")
@@ -73,17 +87,19 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = rename_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = rename_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = rename_function(
+        find_registration_button_on_login_page, page_url, button_text
+    )
     assert (
         actual_result
         == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
